f8e55d1985a2040d3eb2a8677613fe4614350c93
NguyenHuuSang04 <24huusang6a2@gmail.com>
2025-01-20 15:13:26 +0700

    Bài 1: Làm quen C# tiếp theo ( if else, for )

--- C_Sharp/Bai_1_LamQuen/Program.cs
+++ C_Sharp/Bai_1_LamQuen/Program.cs
@@ -1,4 +1,147 @@
-﻿using System;
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +153,48 @@ namespace Bai_1_LamQuen
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("KTHH");
+            int age = 20;
+            if (age > 0)
+            {
+                Console.WriteLine("Bạn là người lớn");
+            }
+            else
+            {
+                Console.WriteLine("Bạn chưa là người lớn");
+            }
+
+            int day = 3;
+            switch(day)
+            {
+                case 0:
+                    Console.WriteLine("Chủ nhật");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                default:
+                    Console.WriteLine("Orther day");
+                    break;
+            }
+
+            //Vòng lặp for
+            for (int i = 0;i < 10; i++)
+            {
+                //Console.WriteLine("Number:{0}", i); // String interpolation
+                Console.WriteLine($"Number: {i}"); // Placeholder ( String format )
+            }
+
+
             Console.ReadKey();
         }
     }
 }
+
